4098b096492a93555a949c84dc263dfdc21abc41
Lane Lu <lane.cn@gmail.com>
2022-07-05 09:58:02 +0800

    feat: export Preview component

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+import FlatListSlider from './src/FlatListSlider';
+import Preview from './src/Preview';
+export { FlatListSlider,  Preview};
